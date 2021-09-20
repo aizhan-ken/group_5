d92a2aa4208460a4b1d54b8fa2a91c926bc73f43
aizhan-ken <aizhanlee93@gmail.com>
2021-09-20 19:37:11 -0400

    1st try

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Designer", "idk", "Student", "Sleep deprived"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });   
+});
